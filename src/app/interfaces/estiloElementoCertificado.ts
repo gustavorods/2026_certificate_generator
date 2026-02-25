422a863eef3aa28fd5f765ec2eeb25eee5ac168b
gustavorods <gustavo.rlsilva07@gmail.com>
2026-02-25 18:58:19 -0300

    fix: resolution certificate page problem on mobile phones

---
+++ src/app/interfaces/estiloElementoCertificado.ts
@@ -0,0 +1,6 @@
+export interface estiloElementoCertificado {
+  elemento: HTMLElement;
+  className: string;
+  width: string;
+  maxWidth: string;
+}
--- src/app/pages/certificado/certificado.ts
+++ src/app/pages/certificado/certificado.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, RouterLink } from "@angular/router";
 import { CertificadoService } from '../../_services/certificado';
 import { Certificado as CertificadoInterface } from '../../interfaces/certificado';
 import html2canvas from 'html2canvas';
+import { estiloElementoCertificado } from '../../interfaces/estiloElementoCertificado';
 
 @Component({
   selector: 'app-certificado',
@@ -14,8 +15,12 @@ import html2canvas from 'html2canvas';
 export class Certificado {
   id: String | null = null;
   certificado: CertificadoInterface | undefined;
+  elementosHTML: estiloElementoCertificado[] = [];
+  elementosHtmlEstiloAntigo: estiloElementoCertificado[] = [];
 
   @ViewChild('certificadoContainer') certificadoElement!: ElementRef;
+  @ViewChild('conteudoCertificado') conteudoCertificadoElement!: ElementRef;
+  @ViewChild('listaAtividades') listaAtividadesElement!: ElementRef;
 
   constructor(private certificadoService: CertificadoService, private route: ActivatedRoute) {}
 
@@ -26,7 +31,61 @@ export class Certificado {
     })
   }
 
+  // Alimentar meu array com os elementos HTML
+  ngAfterViewInit(): void {
+    const elementos = [
+      this.certificadoElement.nativeElement,
+      this.conteudoCertificadoElement.nativeElement,
+      this.listaAtividadesElement.nativeElement
+    ];
+
+    this.elementosHTML = elementos.map(el => {
+      const estilo = getComputedStyle(el);
+      return {
+        elemento: el,
+        className: el.className,
+        width: estilo.width,
+        maxWidth: estilo.maxWidth
+      }
+    });
+  }
+
+  /* Salva as configurações de estilo atuais dos elementos HTML para que eu possa aplicar o estilo
+  necessário para o certificado e depois voltar para o estilo antigo */
+  pegarEstiloAntigo() {
+    this.elementosHtmlEstiloAntigo = this.elementosHTML.map(el => {
+      return {
+        elemento: el.elemento,
+        className: el.className,
+        width: el.elemento.style.width,
+        maxWidth: el.elemento.style.maxWidth
+      }
+    })
+  }
+
+  // Essa função aplica o estilo que eu preciso que o certificado tenha na hora gerar ele
+  aplicarEstiloCertificado() {
+    this.elementosHTML.forEach(el => {
+      el.elemento.classList.remove('w-100');
+      el.elemento.style.width = el.maxWidth;
+      el.elemento.style.maxWidth = 'none';
+    })
+  }
+
+  // Essa função volta o estilo dos elementos HTML para o estilo que eles tinham antes de gerar o certificado (Util para mobile)
+  voltarEstiloAntigo() {
+    this.elementosHtmlEstiloAntigo.forEach(el => {
+      el.elemento.classList.add('w-100');
+      el.elemento.style.width = el.width;
+      el.elemento.style.maxWidth = el.maxWidth;
+      el.elemento.style.width = "none";
+    })
+  }
+
   downloadCertificado() {
+    this.pegarEstiloAntigo();
+    this.aplicarEstiloCertificado();
+
     if(this.certificado == undefined) {
       return;
     }
@@ -38,5 +97,7 @@ export class Certificado {
         link.click();
       }
     )
+
+    this.voltarEstiloAntigo();
   }
 }
